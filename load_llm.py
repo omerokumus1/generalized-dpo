7e6db7799a7603337ae364d76bb614114e890d3a
Ömer Faruk Okumuş <omerokumus3@gmail.com>
2024-11-11 20:58:40 +0300

    load_llm.py ytu_turkish_llama_8b_v01

--- load_llm.py
+++ load_llm.py
@@ -1,16 +1,18 @@
+import torch
 from unsloth import FastLanguageModel
 
 from args import Args
 from supported_llms import LLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
 
-def load_llm(llm_name: LLM):
-    if llm_name.value.startswith("unsloth/"):
+def load_llm(llm: LLM):
+    if llm.value.startswith("unsloth/"):
         max_seq_length = Args.max_seq_length  # Choose any! We auto support RoPE Scaling internally!
         dtype = None  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
         load_in_4bit = True  # Use 4bit quantization to reduce memory usage. Can be False.
         model, tokenizer = FastLanguageModel.from_pretrained(
-            model_name=llm_name.value,
+            model_name=llm.value,
             max_seq_length=max_seq_length,
             dtype=dtype,
             load_in_4bit=load_in_4bit,
@@ -30,4 +32,28 @@ def load_llm(llm_name: LLM):
             use_rslora=False,  # We support rank stabilized LoRA
             loftq_config=None,  # And LoftQ
         )
+        return model, tokenizer
+
+    elif llm == LLM.ytu_turkish_llama_8b_v01:
+        bnb_config = BitsAndBytesConfig(
+            load_in_8bit=True,
+            bnb_8bit_compute_dtype=torch.bfloat16,
+            load_in_8bit_fp32_cpu_offload=True,
+            device_map='auto'
+        )
+
+        tokenizer = AutoTokenizer.from_pretrained(llm.value)
+        model = AutoModelForCausalLM.from_pretrained(
+            llm.value,
+            device_map="auto",
+            torch_dtype=torch.bfloat16,
+            quantization_config=bnb_config,
+        )
+
+        return model, tokenizer
+
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(llm.value)
+        model = AutoModelForCausalLM.from_pretrained(llm.value)
+        model.to(Args.device)
         return model, tokenizer
