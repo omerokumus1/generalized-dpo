ba5f3c3b8520ea1d201f9bac9746daafac6056ac
Ömer Faruk Okumuş <omerokumus3@gmail.com>
2024-12-03 09:22:52 +0300

    supported_llms.py ytu_turkish_gpt2_large removed

--- supported_llms.py
+++ supported_llms.py
@@ -4,5 +4,5 @@ from enum import Enum
 class LLM(Enum):
     unsloth_llama_3_8b_bnb_4bit = "unsloth/llama-3-8b-bnb-4bit"
     unsloth_llama_3_1b_bnb_4bit = "unsloth/Llama-3.2-1B-Instruct-bnb-4bit"
-    ytu_turkish_gpt2_large = "ytu-ce-cosmos/turkish-gpt2-large"
+    # ytu_turkish_gpt2_large = "ytu-ce-cosmos/turkish-gpt2-large"
     ytu_turkish_llama_8b_v01 = "ytu-ce-cosmos/Turkish-Llama-8b-v0.1"
