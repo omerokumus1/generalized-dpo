c83a9c23310de435768ed7e9fa514f88197e7e34
Ömer Okumuş <omerokumus3@gmail.com>
2024-12-06 22:18:58 +0300

    training_llm.py try-except added in case if exception occurs, do not lose the current trained llm

--- training_llm.py
+++ training_llm.py
@@ -6,6 +6,7 @@ from dpo_loss import compute_dpo_loss_batch, evaluate_dpo_loss_loader, dummy_los
 from prepare_dataset import format_input
 from utils import generate_and_print_sample
 from torch.optim import Optimizer
+import traceback
 
 
 def train_model_dpo_simple(
@@ -26,53 +27,55 @@ def train_model_dpo_simple(
     tokens_seen, global_step = 0, -1
 
     # Main training loop
-    for epoch in range(num_epochs):
-        policy_model.train()  # Set model to training mode
-        loss = None
-        for batch_idx, batch in enumerate(train_loader):
-            batch: ProcessedBatch = batch
-            optimizer.zero_grad()  # Reset loss gradients from previous batch iteration
+    try:
+        for epoch in range(num_epochs):
+            policy_model.train()  # Set model to training mode
+            loss = None
+            for batch_idx, batch in enumerate(train_loader):
+                batch: ProcessedBatch = batch
+                optimizer.zero_grad()  # Reset loss gradients from previous batch iteration
 
-            loss, chosen_rewards, rejected_rewards = compute_dpo_loss_batch(
-                batch=batch,
-                policy_model=policy_model,
-                reference_model=reference_model,
-                beta=beta
-            )
-            loss.backward()  # Calculate loss gradients
-            optimizer.step()  # Update model weights using loss gradients
-
-            tokens_seen += batch["chosen"].numel()
-            global_step += 1
-
-            # Optional evaluation step
-            if global_step % eval_freq == 0:
-                res = evaluate_dpo_loss_loader(
+                loss, chosen_rewards, rejected_rewards = compute_dpo_loss_batch(
+                    batch=batch,
                     policy_model=policy_model,
                     reference_model=reference_model,
-                    train_loader=train_loader,
-                    val_loader=val_loader,
-                    beta=beta,
-                    eval_iter=eval_iter
+                    beta=beta
                 )
-                tracking["train_losses"].append(res["train_loss"])
-                tracking["train_chosen_rewards"].append(res["train_chosen_reward"])
-                tracking["train_rejected_rewards"].append(res["train_rejected_reward"])
-                tracking["val_losses"].append(res["val_loss"])
-                tracking["val_chosen_rewards"].append(res["val_chosen_reward"])
-                tracking["val_rejected_rewards"].append(res["val_rejected_reward"])
-                tracking["tokens_seen"].append(tokens_seen)
-                train_reward_margin = res["train_chosen_reward"] - res["train_rejected_reward"]
-                val_reward_margin = res["val_chosen_reward"] - res["val_rejected_reward"]
+                loss.backward()  # Calculate loss gradients
+                optimizer.step()  # Update model weights using loss gradients
 
-                print(
-                    f"Ep {epoch + 1} (Step {global_step:06d}): "
-                    f"Train loss {res['train_loss']:.3f}, Val loss {res['val_loss']:.3f}, "
-                    f"Train reward margins {train_reward_margin:.3f}, "
-                    f"Val reward margins {val_reward_margin:.3f}"
-                )
-                if global_step >= 300:
-                    break
+                tokens_seen += batch["chosen"].numel()
+                global_step += 1
+
+                # Optional evaluation step
+                if global_step % eval_freq == 0:
+                    res = evaluate_dpo_loss_loader(
+                        policy_model=policy_model,
+                        reference_model=reference_model,
+                        train_loader=train_loader,
+                        val_loader=val_loader,
+                        beta=beta,
+                        eval_iter=eval_iter
+                    )
+                    tracking["train_losses"].append(res["train_loss"])
+                    tracking["train_chosen_rewards"].append(res["train_chosen_reward"])
+                    tracking["train_rejected_rewards"].append(res["train_rejected_reward"])
+                    tracking["val_losses"].append(res["val_loss"])
+                    tracking["val_chosen_rewards"].append(res["val_chosen_reward"])
+                    tracking["val_rejected_rewards"].append(res["val_rejected_reward"])
+                    tracking["tokens_seen"].append(tokens_seen)
+                    train_reward_margin = res["train_chosen_reward"] - res["train_rejected_reward"]
+                    val_reward_margin = res["val_chosen_reward"] - res["val_rejected_reward"]
+
+                    print(
+                        f"Ep {epoch + 1} (Step {global_step:06d}): "
+                        f"Train loss {res['train_loss']:.3f}, Val loss {res['val_loss']:.3f}, "
+                        f"Train reward margins {train_reward_margin:.3f}, "
+                        f"Val reward margins {val_reward_margin:.3f}"
+                    )
+    except:
+        traceback.print_exc()
+        return tracking
 
     return tracking
 
