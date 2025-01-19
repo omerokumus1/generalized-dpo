56e12b00ac435e2de8168ade0af021a85cbb56eb
Ömer Okumuş <omerokumus3@gmail.com>
2025-01-19 17:23:00 +0300

    supported_llms.py unsloth_llama_3_3b_instruct

--- supported_llms.py
+++ supported_llms.py
@@ -4,6 +4,7 @@ from enum import Enum
 class LLM(Enum):
     unsloth_llama_3_8b_bnb_4bit = "unsloth/llama-3-8b-bnb-4bit"
     unsloth_llama_3_3b_instruct_bnb_4bit = "unsloth/Llama-3.2-3B-Instruct-bnb-4bit"
+    unsloth_llama_3_3b_instruct = "unsloth/Llama-3.2-3B-Instruct"
     unsloth_llama_3_1b_bnb_4bit = "unsloth/Llama-3.2-1B-Instruct-bnb-4bit"
     # ytu_turkish_gpt2_large = "ytu-ce-cosmos/turkish-gpt2-large"
     ytu_turkish_llama_8b_v01 = "ytu-ce-cosmos/Turkish-Llama-8b-v0.1"
