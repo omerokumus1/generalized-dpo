2ae739f5a2e7d9073f751b4b98a4f5ff7a3b83f2
Ömer Faruk Okumuş <omerokumus3@gmail.com>
2025-01-21 16:40:08 +0300

    learning_rate move to args.py and set to 1e-4

--- args.py
+++ args.py
@@ -19,6 +19,7 @@ class Args:
 
     max_seq_length: int = 1024
     max_context_length: int = 1024
+    learning_rate = 1e-4
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     data_device = torch.device("cpu")
