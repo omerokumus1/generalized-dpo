e652b394f2eabcd68518de6e83945b75242a4729
Ömer Okumuş <omerokumus3@gmail.com>
2025-02-03 11:02:52 +0300

    DpoEntryDict, DpoProcessedBatch and  DpoBatchEntry are added

--- custom_types.py
+++ custom_types.py
@@ -11,6 +11,13 @@ class EntryDict(TypedDict):
     chosen: str
     rejecteds: List[str]
 
+class DpoEntryDict(TypedDict):
+    instruction: str
+    input: str
+    output: str
+    chosen: str
+    rejected: str
+
 
 class ProcessedBatch(TypedDict):
     prompt: List[Tensor]  # List of tensor
@@ -19,8 +26,21 @@ class ProcessedBatch(TypedDict):
     rejecteds_mask: List[List[Tensor]]  # Finally becomes Tensor with shape (batch_size, num_rejecteds, max_length)
     chosen_mask: List[Tensor]  # Finally becomes Tensor with shape (batch_size, max_length)
 
+class DpoProcessedBatch(TypedDict):
+    prompt: List[Tensor]
+    chosen: List[Tensor]
+    rejected: List[Tensor]
+    rejected_mask: List[List[Tensor]]  # Finally becomes Tensor with shape (batch_size, num_rejecteds, max_length)
+    chosen_mask: List[Tensor]  # Finally becomes Tensor with shape (batch_size, max_length)
+
 
 class BatchEntry(TypedDict):
     prompt: List[int]
     chosen: List[int]
     rejecteds: List[List[int]]
+
+
+class DpoBatchEntry(TypedDict):
+    prompt: List[int]
+    chosen: List[int]
+    rejected: List[int]
