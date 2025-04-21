0c6d331031809103d2ec32af17caf037565e6be3
Ömer Okumuş <omerokumus3@gmail.com>
2025-04-21 09:02:44 +0300

    loss_commons.py comments

--- loss_commons.py
+++ loss_commons.py
@@ -4,6 +4,8 @@ from torch import Tensor
 
 from test_util import print_raw_loss_values, print_reward_margins, print_log_prob_range, compare_with_cross_entropy_loss
 
+clamp_max, clamp_min = 20, -20
+
 
 # Stable smoothed logsigmoid loss
 # def smooth_loss(logits, beta, smoothing=0.1):
@@ -33,20 +35,21 @@ def compute_dpo_loss(
     Returns:
         A tuple of three tensors: (loss, chosen_rewards, rejected_rewards).
     """
-    clamp_max,clamp_min = 1e6, -1e6
 
     # Compute log-ratios
     policy_logratios = policy_chosen_logprobs - policy_rejected_logprobs
-    policy_logratios = torch.clamp(policy_logratios, clamp_min, clamp_max)
+    # policy_logratios = torch.clamp(policy_logratios, clamp_min, clamp_max)
 
     reference_logratios = reference_chosen_logprobs - reference_rejected_logprobs
-    reference_logratios = torch.clamp(reference_logratios, clamp_min, clamp_max)
+    # reference_logratios = torch.clamp(reference_logratios, clamp_min, clamp_max)
 
     logits = policy_logratios - reference_logratios
-    logits = torch.clamp(logits, clamp_min, clamp_max)
+    # logits = torch.clamp(logits, clamp_min, clamp_max)
 
     # DPO (Eq. 7 of https://arxiv.org/pdf/2305.18290.pdf)
     losses = -F.logsigmoid(beta * logits)
+    # TODO check the range of losses, which should be (-inf, 0] theoretically, [-20, 0] in practice
+    # * apply clamping with[-20, 0] if numerical instability occurs
     # print_raw_loss_values(losses)
 
     # Detach rewards for monitoring
@@ -74,11 +77,12 @@ def compute_logprobs(logits: Tensor, labels: Tensor, selection_mask: Tensor = No
     # Labels are the inputs shifted by one
     labels = labels[:, 1:].clone()
 
-    # Normalize logits for numerical stability
-    # This normalization step ensures that the largest value in the
-    # logits tensor is 0, reducing the risk of overflow or underflow.
-    logits = logits - logits.max(dim=-1, keepdim=True).values
+    # * PyTorch handles all the numerical stability and internal calculations,
+    # * making it easier and less error-prone.
+
     # Truncate logits to match the labels num_tokens
+    # TODO check if logits contains inf or nan, which should be [-10, 10].
+    # * apply clamping with [-20, 20] if numerical instability occurs
     logits = logits[:, :-1, :]
 
     # Normally, Logit values can be negative, positive, or zero. Softmax will distribute them over [0, 1]
@@ -86,6 +90,8 @@ def compute_logprobs(logits: Tensor, labels: Tensor, selection_mask: Tensor = No
     # and summing them results in 1
     # log_probs values are negative (over (-inf, 0]) since log function
     log_probs = F.log_softmax(logits, dim=-1)
+    # TODO check the range of log_probs, which should be (-inf, 0] theoretically, [-20, 0] in practice
+    # * apply clamping with [-20, 0] if numerical instability occurs
     print_log_prob_range(log_probs)
 
     # Gather the log probabilities for the actual labels
@@ -103,9 +109,10 @@ def compute_logprobs(logits: Tensor, labels: Tensor, selection_mask: Tensor = No
         # Apply the mask to filter out padding tokens
         selected_log_probs = selected_log_probs * mask
 
-        # Calculate the average log probability excluding padding tokens
+        # Calculate the average log probability excluding padding tokens by mask.sum(-1)
         # This averages over the tokens, so the shape is (batch_size, num_tokens)
         # Compute valid tokens count
+        # !! If valid_tokens is 0, it means no active token is in there- all tokens are padding or only prompt tokens
         valid_tokens = mask.sum(-1)
 
         # Avoid division by zero
@@ -114,7 +121,7 @@ def compute_logprobs(logits: Tensor, labels: Tensor, selection_mask: Tensor = No
         #     selected_log_probs.sum(-1) / valid_tokens,
         #     torch.tensor(0.0, device=logits.device)
         # )
-        valid_tokens = valid_tokens.clamp(min=1)  # Avoid division by 0
+        # valid_tokens = valid_tokens.clamp(min=1)  # Avoid division by 0
         avg_log_prob = selected_log_probs.sum(-1) / valid_tokens
 
         return avg_log_prob
